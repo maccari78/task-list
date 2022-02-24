440b3b9cf93501054aeb9ee5c744bace743f37f5
Danilo Maccari <maccari78@gmail.com>
2022-02-24 01:36:23 -0300

    db.json complete

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-
-import { Observable, of } from 'rxjs';
+import{HttpClient, HttpHandler} from '@angular/common/http'
+import { from, Observable, of } from 'rxjs';
 import { Task } from '../Task';
 import { TASKS } from '../mock-task';
 
@@ -8,11 +8,13 @@ import { TASKS } from '../mock-task';
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:5000/tasks'
 
-  constructor() { }
+  constructor(
+    private http:HttpClient
+  ) { }
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl)
   }
 }
